7166232537562217e58a60d59097142bd3d0f0f8
Katherine1702 <valeskaes.rodriguez17@gmail.com>
2022-03-21 12:31:59 -0600

    Add project files.

---
+++ Pages/Contacto.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_GRUPO_UNO.Pages
+{
+    public class ContactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Perfil.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_GRUPO_UNO.Pages
+{
+    public class PerfilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
